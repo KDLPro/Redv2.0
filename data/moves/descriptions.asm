8f34799ed0f70653df730782cbf396f94748d87c
KDLPro <38602758+KDLPro@users.noreply.github.com>
2021-10-20 18:06:58 +0800

    Tweak descriptions

--- data/moves/descriptions.asm
+++ data/moves/descriptions.asm
@@ -557,8 +557,8 @@ LeechSeedDescription:
 	next "foe on every turn.@"
 
 GrowthDescription:
-	db   "Raises the SPCL."
-	next "ATK rating.@"
+	db   "Raises the ATK"
+	next "and SP. ATK.@"
 
 RazorLeafDescription:
 	db   "Has a high criti-"
