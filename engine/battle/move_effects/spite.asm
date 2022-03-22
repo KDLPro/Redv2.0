0e9f16254ab5a31e6690d106022ea862fd31778a
KDLPro <38602758+KDLPro@users.noreply.github.com>
2022-03-22 15:34:47 +0800

    Spite now reduces 5 PP from all moves

--- engine/battle/move_effects/spite.asm
+++ engine/battle/move_effects/spite.asm
@@ -4,77 +4,23 @@ BattleCommand_Spite:
 	ld a, [wAttackMissed]
 	and a
 	jp nz, .failed
-	ld bc, PARTYMON_STRUCT_LENGTH ; unused
-	ld hl, wEnemyMonMoves
-	ldh a, [hBattleTurn]
-	and a
-	jr z, .got_moves
-	ld hl, wBattleMonMoves
-.got_moves
-	ld a, BATTLE_VARS_LAST_COUNTER_MOVE_OPP
-	call GetBattleVar
-	and a
-	jr z, .failed
-	cp STRUGGLE
-	jr z, .failed
-	ld b, a
-	ld c, -1
-.loop
-	inc c
-	ld a, [hli]
-	cp b
-	jr nz, .loop
-	ld [wNamedObjectIndex], a
-	dec hl
-	ld b, 0
-	push bc
-	ld c, wBattleMonPP - wBattleMonMoves
-	add hl, bc
-	pop bc
+	ld hl, wBattleMonPP
 	ld a, [hl]
-	and PP_MASK
-	jr z, .failed
-	push bc
-	call GetMoveName
-	; lose 2 PP
-	ld b, 3
-	ld a, [hl]
-	and PP_MASK
-	cp b
-	jr nc, .deplete_pp
-	ld b, a
-.deplete_pp
-	ld a, [hl]
-	sub b
+	ld b, NUM_MOVES + 1
+.loop_moves
+	dec b
+	jr z, .done
+	
+	cp 5
+	jr nc, .subtract_pp
+	ld a, 5
+.subtract_pp
+	sub 5
 	ld [hl], a
-	push af
-	ld a, MON_PP
-	call OpponentPartyAttr
-	ld d, b
-	pop af
-	pop bc
-	add hl, bc
-	ld e, a
-	ld a, BATTLE_VARS_SUBSTATUS5_OPP
-	call GetBattleVar
-	bit SUBSTATUS_TRANSFORMED, a
-	jr nz, .transformed
-	ldh a, [hBattleTurn]
-	and a
-	jr nz, .not_wildmon
-	ld a, [wBattleMode]
-	dec a
-	jr nz, .not_wildmon
-	ld hl, wWildMonPP
-	add hl, bc
-.not_wildmon
-	ld [hl], e
-.transformed
-	push de
-	call AnimateCurrentMove
-	pop de
-	ld a, d
-	ld [wTextDecimalByte], a
+	inc hl
+	jr .loop_moves
+
+.done
 	ld hl, SpiteEffectText
 	jp StdBattleTextbox
 
