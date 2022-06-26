cd63e4658bce4c0c236bf4b457f820838d046032
KDLPro <38602758+KDLPro@users.noreply.github.com>
2022-06-26 22:57:02 +0800

    Change EXP. formula
    Pokémon need approximately twice the EXP. needed to reach Lv. 100

--- data/growth_rates.asm
+++ data/growth_rates.asm
@@ -11,9 +11,9 @@ ENDM
 
 GrowthRates:
 ; entries correspond to GROWTH_* (see constants/pokemon_data_constants.asm)
-	growth_rate 1, 1,   0,   0,   0 ; Medium Fast
-	growth_rate 3, 4,  10,   0,  30 ; Slightly Fast
-	growth_rate 3, 4,  20,   0,  70 ; Slightly Slow
-	growth_rate 6, 5, -15, 100, 140 ; Medium Slow
-	growth_rate 4, 5,   0,   0,   0 ; Fast
-	growth_rate 5, 4,   0,   0,   0 ; Slow
+	growth_rate 2,  1,   0,   0,   0 ; Medium Fast
+	growth_rate 3,  4,  10,   0,  30 ; Slightly Fast
+	growth_rate 3,  4,  20,   0,  70 ; Slightly Slow
+	growth_rate 13, 5, -50, 100, 140 ; Medium Slow
+	growth_rate 8,  5,   0,   0,   0 ; Fast
+	growth_rate 10, 4,   0,   0,   0 ; Slow
