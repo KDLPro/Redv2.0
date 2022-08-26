d6405f35c650e432f8d4b7769e81867caaa8eee5
KDLPro <38602758+KDLPro@users.noreply.github.com>
2022-08-26 23:24:01 +0800

    Create two_turn_move_effects.asm that AIDamageCalc uses to consider half the BP of moves for damage calculation

---
+++ data/battle/ai/two_turn_move_effects.asm
@@ -0,0 +1,11 @@
+; AIDamageCalc uses half the BP for moves
+; with these effects.
+
+TwoTurnEffects:
+    db EFFECT_SKULL_BASH
+    db EFFECT_FLY
+    db EFFECT_SOLARBEAM
+    db EFFECT_SKY_ATTACK
+    ; db EFFECT_BOUNCE
+    db -1 ; end
+    
